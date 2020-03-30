49169d8dd124f63db665a1e96570f82fbb8b4dfe
Talhabinahmed255 <Talhabinahmed255@gmail.com>
2020-03-30 23:34:11 +0500

    First Commit

---
+++ test.py
@@ -0,0 +1,24 @@
+import glob
+from PIL import Image
+
+widthOfNewImage = 400
+
+counterUsedAsNewNameOfImages=0
+pathOfImagesContainingFolder='C:\\Users\Talha\Desktop\Pics With Qasim\*.jpg'
+newImagesContainingFolder=r'C:\\Users\Talha\Desktop\newFolder\ '
+images = glob.glob(pathOfImagesContainingFolder)
+
+for image in images:
+
+    with open(image, 'rb') as file:
+
+        img = Image.open(file)
+
+        wpercent = (widthOfNewImage / float(img.size[0]))
+        heightOfNewImage = int((float(img.size[1]) * float(wpercent)))
+        print(heightOfNewImage)
+        img = img.resize((widthOfNewImage, heightOfNewImage), Image.ANTIALIAS)
+
+        img.save(newImagesContainingFolder+ str(counterUsedAsNewNameOfImages) + '.jpg' , 'JPEG', quality=100) #Path Of The folder where the resulting images will be stored
+        counterUsedAsNewNameOfImages+=1
+
