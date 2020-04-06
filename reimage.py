70b64c88bf39c3a0afa0f9c96e48ba9718fdd788
HussainTaj-W <hussaintajwork@gmail.com>
2020-04-06 13:20:18 +0500

    Refactor Code
    Add module import scenario.
    Add Config class
    Add ToDos
    Add A Scrap Snippet

--- reimage.py
+++ reimage.py
@@ -1,24 +1,45 @@
-import glob
-from PIL import Image
+class Config:
+    # TODO add Doc string
+    pass
 
-widthOfNewImage = 400
 
-counterUsedAsNewNameOfImages=0
-pathOfImagesContainingFolder='C:\\Users\Talha\Desktop\Pics With Qasim\*.jpg'
-newImagesContainingFolder=r'C:\\Users\Talha\Desktop\newFolder\ '
-images = glob.glob(pathOfImagesContainingFolder)
+def reimage(config: Config):
+    # TODO add Doc string
 
-for image in images:
+    import glob
+    from PIL import Image
 
-    with open(image, 'rb') as file:
+    widthOfNewImage = 400
 
-        img = Image.open(file)
+    counterUsedAsNewNameOfImages=0
+    pathOfImagesContainingFolder='C:\\Users\Talha\Desktop\Pics With Qasim\*.jpg'
+    newImagesContainingFolder=r'C:\\Users\Talha\Desktop\newFolder\ '
+    images = glob.glob(pathOfImagesContainingFolder)
 
-        wpercent = (widthOfNewImage / float(img.size[0]))
-        heightOfNewImage = int((float(img.size[1]) * float(wpercent)))
-        print(heightOfNewImage)
-        img = img.resize((widthOfNewImage, heightOfNewImage), Image.ANTIALIAS)
+    for image in images:
 
-        img.save(newImagesContainingFolder+ str(counterUsedAsNewNameOfImages) + '.jpg' , 'JPEG', quality=100) #Path Of The folder where the resulting images will be stored
-        counterUsedAsNewNameOfImages+=1
+        with open(image, 'rb') as file:
 
+            img = Image.open(file)
+
+            wpercent = (widthOfNewImage / float(img.size[0]))
+            heightOfNewImage = int((float(img.size[1]) * float(wpercent)))
+            print(heightOfNewImage)
+            img = img.resize((widthOfNewImage, heightOfNewImage), Image.ANTIALIAS)
+
+            img.save(newImagesContainingFolder+ str(counterUsedAsNewNameOfImages) + '.jpg' , 'JPEG', quality=100) #Path Of The folder where the resulting images will be stored
+            counterUsedAsNewNameOfImages+=1
+
+
+if __name__ == '__main__':
+
+    # TODO configure and parse args
+
+    config = Config()  # TODO make config from args
+    reimage(config=config)
+
+
+# Scrap Code Snippets
+import os
+for path, directories, files in os.walk("."):
+    break
